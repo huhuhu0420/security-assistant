3253358b8a2e2f78cfdc5f10907b7f83fa032016
huhuhu0420 <ghostcheng.00@gmail.com>
2023-06-06 00:18:37 +0800

    know how to use slash command

--- bot.py
+++ bot.py
@@ -2,17 +2,24 @@
 import discord
 from bottoken import TOKEN
 from discord.ext import commands
+from discord import app_commands
 
 # client是跟discord連接，intents是要求機器人的權限
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents = intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=919254033938665503)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")
 
 # 調用event函式庫
 @client.event
 # 當機器人完成啟動
 async def on_ready():
-    print(f"目前登入身份 --> {client.user}")
+    await tree.sync(guild=discord.Object(id=919254033938665503))
+    print(f"ready --> {client.user}")
 
 @client.event
 # 當頻道有新訊息
