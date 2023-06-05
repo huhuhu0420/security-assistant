a9aa7e81f89f204b3553feed9a6ccee306750248
huhuhu0420 <ghostcheng.00@gmail.com>
2023-06-06 00:20:17 +0800

    server id

--- bot.py
+++ bot.py
@@ -4,13 +4,15 @@ from bottoken import TOKEN
 from discord.ext import commands
 from discord import app_commands
 
+serverid = 919254033938665503
+
 # client是跟discord連接，intents是要求機器人的權限
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents = intents)
 tree = app_commands.CommandTree(client)
 
-@tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=919254033938665503)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@tree.command(name = "commandname", description = "My first application Command", guild=discord.Object(id=serverid)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
 
@@ -18,7 +20,7 @@ async def first_command(interaction):
 @client.event
 # 當機器人完成啟動
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=919254033938665503))
+    await tree.sync(guild=discord.Object(id=serverid))
     print(f"ready --> {client.user}")
 
 @client.event
